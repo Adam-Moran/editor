e7f1e1166a277b02ade03911ffddda515a096446
Adam-Moran <adam.moran57@gmail.com>
2021-12-15 20:53:01 +0000

    Initial

---
+++ addon/components/editor.js
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+import { CodeMirror } from 'codemirror'; 
+
+export default class DeviceDownloadComponent extends Component {
+    constructor(){
+        console.log("a");
+        super(...arguments);
+    }
+}
